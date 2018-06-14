de53a5238a91541271377a2a4510ce640b3e8b83
西门吸雪 <renhanlinwy@163.com>
2018-06-14 18:05:17 +0800

    commit2018年06月14日18:05:14

--- HLLittleDream/HLClass/Two/Multithreading/HLGCDVC.m
+++ HLLittleDream/HLClass/Two/Multithreading/HLGCDVC.m
@@ -52,19 +52,41 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    // @"并发队列-同步",@"并发队列-异步",@"串行队列-同步",@"串行队列-异步",@"",
     [self.view makeToast:[self.dataArray objectAtIndex:indexPath.row]];
     if (indexPath.row == 0) {
         [self tableView0:tableView didSelectRowAtIndexPath:indexPath];
+    }else if (indexPath.row == 1){
+        [self tableView1:tableView didSelectRowAtIndexPath:indexPath];
     }
 }
 
 //同步-并发
 - (void)tableView0:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    //先创建队列
+    dispatch_queue_t fistyQueue = dispatch_queue_create("test_asyncqueue", DISPATCH_QUEUE_CONCURRENT);
     
+    dispatch_async(fistyQueue, ^{//异步函数
+        for (int i = 0; i < 3; i ++) {
+            NSLog(@"1 -----> %@",[NSThread currentThread]);
+        }
+    });
+
+    dispatch_async(fistyQueue, ^{
+        for (int i = 0; i <2; i ++) {
+            NSLog(@"2 -----> %@",[NSThread currentThread]);
+        }
+    });
+    
+    //首先创建的是并发队列 DISPATCH_QUEUE_CONCURRENT。在异步函数里面执行，发现结果是按照顺序执行，可知道是
 }
 
-
+//并发-异步
+- (void)tableView1:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
 
 
 
